f1e3ad69e20e192df2fca2716494abb59ab29a14
chris <christianwu56@gmail.com>
2026-07-01 11:47:34 -0700

    dfdfd

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const NAV_ITEMS = [
   { path: "/integrations", label: "Integrations", icon: Zap },
   { path: "/unity-devices", label: "Unity Devices", icon: Cable },
  // { path: "/tello-dashboard", label: "Tello Dashboard", icon: Smartphone },
-  // { path: "/rollercoin-calculator", label: "Rollercoin", icon: Squirrel },
+  { path: "/rollercoin-calculator", label: "Rollercoin", icon: Squirrel },
   { path: "/cloud", label: "Gamble", icon: Volleyball },
 ];
 
